a99968c0318977f6782e92e18cc3f56a23592817
medkhabt <medkha.bt@gmail.com>
2024-01-18 12:00:09 +0100

    Fix the generation of the graphs bug, related to the generation of the dat files.

--- question_1/generate_graphs.sh
+++ question_1/generate_graphs.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # Create the necessary folders  
-temp_folder=$(echo "temp_$RANDOM")
+rm -rf .temp_* 2>/dev/null
+temp_folder=$(echo ".temp_$RANDOM")
 mkdir target 2>/dev/null 
 mkdir stats 2>/dev/null
 mkdir stats/csv stats/dat 2>/dev/null
@@ -11,7 +12,7 @@ mkdir graphs 2>/dev/null
 mkdir graphs/.generator 2>/dev/null
 ## for the dat files that stores the data in the right form for gnuplot before being ploted. 
 mkdir graphs/.generator/.data 2>/dev/null
-mkdir temp_folder 2>/dev/null
+mkdir $temp_folder 2>/dev/null
 
 # Remove training intermediate data from old execution or wrong execution of the script, just to be safe.
 
@@ -22,56 +23,93 @@ rm graphs/.generator/* 2>/dev/null
 rm graphs/.generator/.data/* 2>/dev/null
 rm graphs/* 2>/dev/null
 # TODO whatchout, in case running multiple scripts, maybe create a folder for this bash command 
-rm -rf temp_folder 2>/dev/null
 # TODO URGENT : We are now doing Strong scaling analysis.
 # ls with p so folder will have a slash at the end, and -l so we can have a file/folder per line for awk , grep -v to remove the folders.
-ls src/parallel -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > temp_folder/.parallel_files_to_compile.tmp
-ls src/sequential -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > .sequential_files_to_compile.tmp
+ls src/parallel -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > $temp_folder/.parallel_files_to_compile.tmp
+ls src/sequential -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > $temp_folder/.sequential_files_to_compile.tmp
 
 # Compilation of the parallel files.
 while IFS= read -r line 
 do 
    target_file=$(echo $line | sed 's/.cpp//')
    g++ -o target/$target_file src/parallel/$line src/stats/generate_stats.cpp -fopenmp -O3 -march=native
-   echo "$target_file">>.files_to_run.tmp
-done < .parallel_files_to_compile.tmp
+   echo "$target_file">> $temp_folder/.files_to_run.tmp
+done < $temp_folder/.parallel_files_to_compile.tmp
 # Compilation of the sequential files.
 while IFS= read -r line 
 do 
    target_file=$(echo $line | sed 's/.cpp//')
    g++ -o target/$target_file src/sequential/$line src/stats/generate_stats.cpp -O3 -march=native
-   echo "$target_file">>.files_to_run.tmp
-done < .sequential_files_to_compile.tmp
+   echo "$target_file">> $temp_folder/.files_to_run.tmp
+done < $temp_folder/.sequential_files_to_compile.tmp
 
-
+echo "End of compilation" 
 
 # Create test cases and run them 
-num_steps=(1000000 100000000) #10000000000 1000000000000)
-num_cores=(1, 2, 4, 8)
-num_repeats=1
+num_steps=(1000000 100000000 10000000000 1000000000000)
+num_cores=(1 2 4 8)
+num_repeats=2
 while IFS= read -r line 
 do
+    echo "*********** file: $line *******"
+    # wait $! 
+    echo "**********************************"
+
     for step in ${num_steps[@]}; do 
 	for core in ${num_cores[@]}; do 
-	    for repeat in {0..$num_repeats}; do 
+	    for ((repeat=0; repeat<=$num_repeats; repeat++)) ; do 
+		echo "num_repeats is ${num_repeats}"
+		echo "step ${step}, core ${core}, repeat ${repeat}"
+		let "index = $step * 4 * 10 +  $core * 10 + $repeat"
+		./target/${line} -C ${core} -N ${step} &
+		pids[${index}]=$! 
 # does it work also for the sequential target 
-		./target/${line} -C ${core} -N ${step}
-	    done 
-	done 
+	    done # end repeat loop 
+	done # end core loop 
+    done # end step loop 
+# TODO block here before change the name of the stats.csv ( acutally they were no stats generated as the first execution didn't finish when arriving here so the stats.csv wasn't created.
+    for pid in ${pids[*]};do 
+	wait $pid 
     done 
     mv stats.csv stats/csv/${line}_stats.csv
-done < .files_to_run.tmp
-#TODO remove this exit 
+    # wait $!
+    echo "End tests"
+
+done < $temp_folder/.files_to_run.tmp
 # Create graphs "gnuplot"
 
 ## Create dat files. changing the , by spaces 
-ls stats/csv -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > .csv_stat_files.tmp
+ls stats/csv -pl | grep -v / | awk '{if(NR-1 > 0) print $NF }' > $temp_folder/.csv_stat_files.tmp
 
-#TODO don't forget the avg when we have more than one repeat, we can just divide the summation of all the cputimes on the number of repeats. before doing any speedup , i would need an association array for the sum of the same iteration and an othe one for the number of occurances. maybe also detect if there are some instances that are not present ( 4 out of 5 , one failed!) . 
 while IFS= read -r line 
 do 
-# we assuem that the file is order, if it isn't it wouldn't work with this implementation.
-    sed 's/,/ /g' stats/csv/$line | awk '{if($1==1){start[$2]=$3} print($1,$2,start[$2]/$3)}' > graphs/.generator/.data/${line}_speed_up.dat   
+sed 's/,/ /g' stats/csv/$line | awk '{
+    key = $2 + 50 * $1; 
+    sum[key]+=$3 ; N[key]++; cores[key]=$1; num_repeats[key]=$2; 
+} END {
+    for (k in sum) {
+	avg = sum[k]/N[k];
+	printf "%s %s %f\n", cores[k], num_repeats[k], avg;
+    }
+}'> .tmp_dat_$line
+ awk '
+{
+    if($1==1){
+	start[$2]=$3
+    }
+}
+ END { 
+	while(getline < FILENAME){
+	   print($1, $2, start[$2]/$3)
+	}
+}' .tmp_dat_$line |  sort -k2,2 -k1 > graphs/.generator/.data/${line}_speed_up.dat   
+
+
+
+
+
+
+
 echo -e  "set term png size 1280,720\nset output '../${line}__speed_up.png'\nset logscale y\n set key left top" >> graphs/.generator/generate_${line}_speed_up_plot.dem
 
 echo -n "plot " >> graphs/.generator/generate_${line}_speed_up_plot.dem
@@ -79,14 +117,17 @@ echo -n "plot " >> graphs/.generator/generate_${line}_speed_up_plot.dem
 	# Watch out, it depends on the placement of the dem file. The script expects the dem file to be in the graphs/.generator/
 	echo -n "\".data/${line}_speed_up.dat\" using 1:(\$2==$step ? \$3:NaN) title \"n_iterations: $step\" with lp,"  >> graphs/.generator/generate_${line}_speed_up_plot.dem
 
-# TODO watch out for the last comma, that is effect the plotting ? 	
+# TODO remove the sequential case from the geneartion of the graph ? 
     cd graphs/.generator
+    echo " the line is $line"
     gnuplot generate_${line}_speed_up_plot.dem
     # Get back to the root folder.
     cd ../..
     done
 echo 
-done < .csv_stat_files.tmp
+done < $temp_folder/.csv_stat_files.tmp
+
+echo "End the generation of graphs"
 
 # CREATION OF the dem file to get ploted.
 
